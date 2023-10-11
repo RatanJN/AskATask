44650954243cbffdc67fc138b3b8a49584c6d011
Aman Kumar <amankumar7506@gmail.com>
2023-10-11 15:10:41 -0400

    update task.js

--- server/models/task.js
+++ server/models/task.js
@@ -14,7 +14,7 @@ const taskSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true,
-  },
+  }, 
   accepted_by: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
