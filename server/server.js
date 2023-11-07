289a35adb6a5aa6c4b99d1a47aeaad45cba7cc2c
Aman Kumar <amankumar7506@gmail.com>
2023-11-06 21:28:24 -0500

    add cors

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const { requireLogin } = require("./controllers/auth_controller");
+const cors = require("cors");
 
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
@@ -11,6 +12,7 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
 app.use(express.json());
+app.use(cors());
 
 const authRouter = require("./routes/auth");
 app.use("/", authRouter);
